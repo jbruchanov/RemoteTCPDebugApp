f3ab1b054994e25df955b1b559164af578205895
Jiri Bruchanov <jbruchanov@gmail.com>
2016-12-15 09:29:42 +0000

    Init commit

---
+++ app/src/main/java/com/scurab/android/remotetcpdebug/MainActivity.java
@@ -0,0 +1,48 @@
+package com.scurab.android.remotetcpdebug;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MainActivity extends Activity {
+
+    private TextView mIps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mIps = (TextView) findViewById(R.id.ip);
+        findViewById(R.id.btn_enable_tcp_debug).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onEnableTCPDebug();
+            }
+        });
+        mIps.setText(Arrays.toString(ShellHelper.getIpAddresses().toArray()));
+    }
+
+    protected void onEnableTCPDebug() {
+        String[] cmds = new String[]{
+                "setprop service.adb.tcp.port 5555",
+                "stop adbd",
+                "start adbd",
+        };
+        try {
+            ShellHelper.execute("su", cmds);
+            showToast("OK");
+        } catch (Throwable e) {
+            showToast(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void showToast(String msg) {
+        Toast.makeText(this, msg != null ? msg : "NULL_MSG", Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/scurab/android/remotetcpdebug/ShellHelper.java
@@ -0,0 +1,89 @@
+package com.scurab.android.remotetcpdebug;
+import org.apache.commons.io.IOUtils;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by JBruchanov on 25/10/2016.
+ */
+
+public class ShellHelper {
+
+    public static String test() {
+        return executeSafe("su", "mkdir /sdcard/testdir");
+    }
+
+    public static String executeSafe(String cmd, String... cmds) {
+        String result = null;
+        try {
+            result = execute(cmd, cmds);
+        } catch (Throwable e) {
+            StringWriter stack = new StringWriter();
+            e.printStackTrace(new PrintWriter(stack));
+            result = e.getMessage() + "\n" + stack.toString();
+        }
+
+        return result;
+    }
+
+    public static String execute(String cmd, String... cmds) throws IOException, InterruptedException {
+        Process p = Runtime.getRuntime().exec(cmd);
+        DataOutputStream dos = new DataOutputStream(p.getOutputStream());
+        for (String c : cmds) {
+            dos.writeBytes(c + "\n");
+        }
+        dos.flush();
+        dos.close();
+        p.waitFor();
+        InputStream inputStream = p.getInputStream();
+        String result = IOUtils.toString(inputStream);
+        if (result == null || result.isEmpty()) {
+            result = "No output detected.";
+        }
+        return result;
+    }
+
+    public static List<String> getIpAddresses() {
+        String consoleOutput = executeSafe("ifconfig");
+        List<String> result = new ArrayList<>();
+        if (consoleOutput != null && consoleOutput.length() > 0) {
+            String[] lines = consoleOutput.split("\\n");
+            Pattern compile = Pattern.compile("inet addr:(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})");
+            for (String line : lines) {
+                Matcher matcher = compile.matcher(line);
+                if (matcher.find()) {
+                    result.add(matcher.group(1));
+                }
+            }
+        }
+
+        if (result.size() == 0) {
+            consoleOutput = executeSafe("netcfg");
+            if (consoleOutput != null && consoleOutput.length() > 0) {
+                String[] lines = consoleOutput.split("\\n");
+                Pattern compile = Pattern.compile("(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})");
+                Set<String> found = new HashSet<>();
+                for (String line : lines) {
+                    Matcher matcher = compile.matcher(line);
+                    if (matcher.find()) {
+                        found.add(matcher.group(1));
+                    }
+                }
+                found.remove("127.0.0.1");
+                found.remove("0.0.0.0");
+                result.addAll(found);
+            }
+        }
+        return result;
+    }
+}
